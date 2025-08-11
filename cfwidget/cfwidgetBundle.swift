f74be56a7d213e3aa27b27e299d7b63e57fb4cf8
JackHowa <jackhowa@fastmail.com>
2025-08-10 20:02:42 -0700

    Add setup for ios widget

---
+++ cfwidget/cfwidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  cfwidgetBundle.swift
+//  cfwidget
+//
+//  Created by Jack Howard on 8/10/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct cfwidgetBundle: WidgetBundle {
+    var body: some Widget {
+        cfwidget()
+        cfwidgetControl()
+        cfwidgetLiveActivity()
+    }
+}
